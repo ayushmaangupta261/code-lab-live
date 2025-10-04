883f659c6fc61435c5ada0efdb809e39c5991635
Ayushmaan Gupta <ayush.261.gupta@gmail.com>
2025-10-05 03:41:52 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,41 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Auth from './pages/Auth.jsx';
-import CreateAndJoinPage from './pages/CreateAndJoinPage.jsx';
-import EditorPage from './pages/EditorPage.jsx';
-import SubmitSolution from './components/Editor/SubmitSolution.jsx';
-import About from './pages/AboutUs.jsx';
-import Dashboard from './pages/Dashboard.jsx';
-import ProtectedRoute from './components/Auth/ProtectedRoute.jsx';
+import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setModal } from "./redux/slices/authSlice";
+import { Outlet } from "react-router-dom";
+import { PeerProvider } from "./providers/PeerProvider";
+import Navbar from "./components/Navbar/Navbar";
 
-// Dashboard Components
-import Overview from './components/Dashboard/DashboardComponent/Overview.jsx';
-import Assignment from './components/Dashboard/DashboardComponent/Assignment.jsx';
-import DailySchedule from './components/Dashboard/DashboardComponent/DailySchedule.jsx';
-import QuestionsSolved from './components/Dashboard/DashboardComponent/QuestionsSolved.jsx';
-import Jobs from './components/Dashboard/DashboardComponent/Jobs.jsx';
-import JobUpdates from './components/Dashboard/DashboardComponent/JobUpdates.jsx';
-import Settings from './components/Dashboard/DashboardComponent/Settings.jsx';
-import Notification from './components/Dashboard/DashboardComponent/Notification.jsx';
-import Projects from './components/Dashboard/DashboardComponent/Projects.jsx';
-import CreateQuestion from './components/Dashboard/DashboardComponent/CreateQuestion.jsx';
-import ViewSolvedQuestions from './components/Dashboard/DashboardComponent/ViewSolvedQuestions.jsx';
-import Students from './components/Dashboard/DashboardComponent/Students.jsx';
-import AssignmentsSolved from './components/Dashboard/DashboardComponent/Instructor-Students/AssignmentsSolved.jsx';
-import StudentProjects from './components/Dashboard/DashboardComponent/Instructor-Students/StudentProjects.jsx';
-import Questions from './components/Dashboard/DashboardComponent/Questions.jsx';
-import ViewSolution from './components/Dashboard/DashboardComponent/Instructor-Students/ViewSolution.jsx';
 
-const App = () => {
+function App() {
+  const modal = useSelector((state) => state.auth.modal);
+  const dispatch = useDispatch();
+  const modalRef = useRef();
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      dispatch(setModal(false));
+    }
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/create-and-join" element={<CreateAndJoinPage />} />
-      <Route path="/editor/:roomId" element={<EditorPage />} />
-      <Route path="/editor/submitSolution" element={<SubmitSolution />} />
-      <Route path="/about-us" element={<About />} />
+    <div
+      ref={modalRef}
+      onClick={closeModal}
+      className="min-h-screen overflow-x-hidden  w-full bg-[#121212] text-white flex flex-col relative"
+    >
+      <PeerProvider>
+        
+        <Navbar />
 
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      >
-        <Route path="overview" element={<Overview />} />
-        <Route path="assignments" element={<Assignment />} />
-        <Route path="daily-schedule" element={<DailySchedule />} />
-        <Route path="questions-solved" element={<QuestionsSolved />} />
-        <Route path="create-question" element={<CreateQuestion />} />
-        <Route path="jobs" element={<Jobs />} />
-        <Route path="job-updates" element={<JobUpdates />} />
-        <Route path="notification" element={<Notification />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="instructor-projects" element={<Students />} />
-        <Route path="solved-assignments/:questionId" element={<AssignmentsSolved />} />
-        <Route path="student-projects" element={<StudentProjects />} />
-        <Route path="questions-Solved/view-solved-questions" element={<ViewSolvedQuestions />} />
-        <Route path="my-questions" element={<Questions />} />
-        <Route path="view-solution/:studentId" element={<ViewSolution />} />
-      </Route>
-    </Routes>
+        
+        <main className="flex-1 mt-8 lg:mt-24 px-4 sm:px-6 lg:px-8">
+          <Outlet />
+        </main>
+
+     
+      </PeerProvider>
+    </div>
   );
-};
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,155 @@
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/index.js";
-import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import EditorPage from "./pages/EditorPage.jsx";
+import CreateAndJoinPage from "./pages/CreateAndJoinPage.jsx";
 import { Toaster } from "react-hot-toast";
+import Auth from "./pages/Auth.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import ProtectedRoute from "./components/Auth/ProtectedRoute.jsx";
+import Projects from "./components/Dashboard/DashboardComponent/Projects.jsx";
+import Overview from "./components/Dashboard/DashboardComponent/Overview.jsx";
+import { Assignment } from "./components/Dashboard/DashboardComponent/Assignment.jsx";
+import DailySchedule from "./components/Dashboard/DashboardComponent/DailySchedule.jsx";
+import QuestionsSolved from "./components/Dashboard/DashboardComponent/QuestionsSolved.jsx";
+import Jobs from "./components/Dashboard/DashboardComponent/Jobs.jsx";
+import { JobUpdates } from "./components/Dashboard/DashboardComponent/JobUpdates.jsx";
+import Settings from "./components/Dashboard/DashboardComponent/Settings.jsx";
+import Notification from "./components/Dashboard/DashboardComponent/Notification.jsx";
+import SubmitSolution from "./components/Editor/SubmitSolution.jsx";
+import CreateQuestion from "./components/Dashboard/DashboardComponent/CreateQuestion.jsx";
+import About from "./pages/AboutUs.jsx";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+import ViewSolvedQuestions from "./components/Dashboard/DashboardComponent/ViewSolvedQuestions.jsx";
+import Students from "./components/Dashboard/DashboardComponent/Students.jsx";
+import AssignmentsSolved from "./components/Dashboard/DashboardComponent/Instructor-Students/AssignmentsSolved.jsx";
+import StudentProjects from "./components/Dashboard/DashboardComponent/Instructor-Students/StudentProjects.jsx";
+import Questions from "./components/Dashboard/DashboardComponent/Questions.jsx";
+import ViewSolution from "./components/Dashboard/DashboardComponent/Instructor-Students/ViewSolution.jsx";
 
-root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-        containerStyle={{ marginTop: "1rem", marginRight: "1rem" }}
-      />
-    </Provider>
-  </BrowserRouter>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/create-and-join",
+        element: <CreateAndJoinPage />,
+      },
+      {
+        path: "/editor/:roomId",
+        element: <EditorPage />,
+      },
+      {
+        path: "/editor/submitSolution",
+        element: <SubmitSolution />,
+      },
+      {
+        path: "/about-us",
+        element: <About />,
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        ),
+        children: [
+          {
+            path: "/dashboard/overview",
+            element: <Overview />,
+          },
+          {
+            path: "/dashboard/assignments",
+            element: <Assignment />,
+          },
+          {
+            path: "/dashboard/daily-schedule",
+            element: <DailySchedule />,
+          },
+          {
+            path: "/dashboard/questions-solved",
+            element: <QuestionsSolved />,
+          },
+          {
+            path: "/dashboard/create-question",
+            element: <CreateQuestion />,
+          },
+          {
+            path: "/dashboard/jobs",
+            element: <Jobs />,
+          },
+          {
+            path: "/dashboard/job-updates",
+            element: <JobUpdates />,
+          },
+          {
+            path: "/dashboard/notification",
+            element: <Notification />,
+          },
+          {
+            path: "/dashboard/projects",
+            element: <Projects />,
+          },
+          {
+            path: "/dashboard/settings",
+            element: <Settings />,
+          },
+          {
+            path: "/dashboard/instructor-projects",
+            element: <Students />,
+          },
+          {
+            path: "/dashboard/solved-assignments/:questionId",
+            element: <AssignmentsSolved />,
+          },
+          {
+            path: "/dashboard/student-projects",
+            element: <StudentProjects />,
+          },
+          {
+            path: "/dashboard/questions-Solved/view-solved-questions",
+            element: <ViewSolvedQuestions />,
+          },
+          {
+            path: "/dashboard/my-questions",
+            element: <Questions />,
+          },
+          {
+            path: "/dashboard/view-solution/:studentId",
+            element: <ViewSolution />,
+          },
+        ],
+      },
+      ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+
+    <Toaster
+      position="top-right"
+      reverseOrder={false}
+      containerStyle={{
+        marginTop: "1rem",
+        marginRight: "1rem",
+      }}
+          />
+  </Provider>
 );
