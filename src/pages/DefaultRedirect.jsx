13e253929aebd3337cef9e77f1e1b362072a1bde
Ayushmaan Gupta <132862778+ayushmaangupta261@users.noreply.github.com>
2026-01-13 00:31:37 +0530

    Create DefaultRedirect.jsx

---
+++ src/pages/DefaultRedirect.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const DefaultRedirect = () => {
+  const user = useSelector((state) => state.auth.user);
+
+  if (user) {
+    return <Navigate to="/dashboard/overview" replace />;
+  }
+
+  return <Navigate to="/" replace />;
+};
+
+export default DefaultRedirect;
